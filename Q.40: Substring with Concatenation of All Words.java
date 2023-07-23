092f2b9cc5e633783b10ffaa69462f0c0f8d0b36
LAKSHMI PRASANNA TANGUTURI <88602508+prasanna61@users.noreply.github.com>
2023-07-23 15:36:36 +0530

    Create Q.40: Substring with Concatenation of All Words.java

---
+++ Q.40: Substring with Concatenation of All Words.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+         if (s.isEmpty() || words.length == 0) {
+      return new ArrayList<>();
+    }
+    List<Integer> indices = new ArrayList<>();
+    Map<String, Integer> wordCounts = new HashMap<>();
+    for (String word : words) {
+      wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+    }
+    Map<String, Integer> currentCounts = new HashMap<>();
+    int wordLength = words[0].length();
+    int totalLength = wordLength * words.length;
+    for (int i = 0; i < s.length() - totalLength + 1; i++) {
+      currentCounts.clear();
+      for (int j = 0; j < words.length; j++) {
+        String word = s.substring(i + j * wordLength, i + (j + 1) * wordLength);
+        currentCounts.put(word, currentCounts.getOrDefault(word, 0) + 1);
+      }
+      if (currentCounts.equals(wordCounts)) {
+        // If they are equal, add the starting index of the current substring to the list of indices
+        indices.add(i);
+      }
+    }
+
+    // Return the list of indices
+    return indices;
+    }
+}
